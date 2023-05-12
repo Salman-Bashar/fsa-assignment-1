e61a51add7f40ed2d2aa0394401c5b82c3bd5d5e
Salman Bashar <salman@lemonhive.com>
2023-05-13 04:15:49 +0600

    Add handleGenerateRandomNumber middleware

---
+++ src/middleware/handle-generate-random-number.js
@@ -0,0 +1,29 @@
+import { generateRandomNumber } from "../generate-random-number.js"
+
+export const handleGenerateRandomNumber = (req, res) => {
+  try {
+    let startNumber, endNumber, result
+
+    if (req.query.start != undefined) {
+      startNumber = parseInt(req.query.start)
+    }
+
+    if (req.query.end != undefined) {
+      endNumber = parseInt(req.query.end)
+    }
+
+    if (req.query.start != undefined && req.query.end != undefined) {
+      result = generateRandomNumber(startNumber, endNumber)
+
+      res.json(
+        `A random number between ${startNumber} & ${endNumber} is ${result}`
+      )
+    } else {
+      res.json(`Please provide valid numbers`)
+    }
+  } catch {
+    res
+      .status(422)
+      .json("Invalid Query Param! Please try again with valid params.")
+  }
+}
