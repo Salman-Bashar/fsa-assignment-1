bd40d808616fbd8ed228a11fb49fbb064178a415
Salman Bashar <salmanbadhon@gmail.com>
2023-05-13 12:00:36 +0600

    Update App.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { handleRandomNumberGenerator } from "./middleware/handle-random-number-generator.js"
 import { handleFakePersonGenerator } from "./middleware/handle-fake-person-generator.js"
+import { handleStringStatsCounter } from "./middleware/handle-string-stats-counter.js"
 
 const app = express()
 const PORT = 4000
@@ -21,4 +22,11 @@ app.get("/api/v1/generate-random-number", (req, res) => {
  */
 app.get("/api/v1/generate-fake-person", (req, res) => {
   handleFakePersonGenerator(req, res)
+})
+
+/**
+ * Count AString Stats
+ */
+app.get("/api/v1/count-string-stats", (req, res) => {
+  handleStringStatsCounter(req, res)
 })
